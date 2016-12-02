db4efeb55f5dbb1b2b17d4e6b3253ed663bf9a5f
Sebastián Águila <saguila@MacBook-Air-de-Sebastian.local>
2016-12-02 15:02:53 +0100

    añadidos modulos

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const MongoClient = require('mongodb').MongoClient,assert = require('assert'); // https://mongodb.github.io/node-mongodb-native/index.html
+const URL_DB = 'mongodb://saboteurApp:3,14016pi@localhost:27017/saboteurDB';
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -13,7 +14,20 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+require('crypto').randomBytes(48, function(err, buffer) {
+  console.log(buffer.toString('hex'))});
+MongoClient.connect(URL_DB, function(err, db) {
+  assert.equal(null, err);
+  if (err) {
+    throw err;
+  }
+  db.collection('test').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+});
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
