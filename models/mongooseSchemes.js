21e16a3d433733c099ba1eb4eb7c55bf362af8c7
Sebastián Águila <saguila@MacBook-Air-de-Sebastian.local>
2016-12-02 01:49:09 +0100

    Algo habre hecho

---
+++ models/mongooseSchemes.js
@@ -0,0 +1,1 @@
+mongooseSchemes.js
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+/* Router pueden considerarse como una mini-aplicacion web con sus propias rutas y su propia cadena midleware 
+tiene metodos use(),get(),post()*/
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Saboteur' });
 });
 
 module.exports = router;
