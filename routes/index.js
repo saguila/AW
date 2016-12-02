59a17f988bc1f2910948cf13fd9171367f84456c
Sebastián Águila <saguila@MacBook-Air-de-Sebastian.local>
2016-12-02 03:38:24 +0100

    s

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-/* Router pueden considerarse como una mini-aplicacion web con sus propias rutas y su propia cadena midleware 
-tiene metodos use(),get(),post()*/
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Saboteur' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
